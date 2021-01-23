2f84cb19ec0f3854861c71b963743f4b48a09642
Jtyrrell0351 <joshua.p.tyrrell@protonmail.com>
2021-01-23 00:50:56 +0000

    add updates

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    principal = document.getElementById("principal").value;
+
     
+    if (principal <=0) {
+        alert("Please enter a valid number. ");
+        document.getElementById("principal").focus();
+    }
+
+    else {
+    rate = document.getElementById("rate").value;
+    
+    var years = parseInt(document.getElementById("years").value);
+    
+    interest = principal*years*rate/100;
+    
+    var today = new Date();
+    
+    var year = parseInt(today.getFullYear());
+    var future_year = year+years;
+
+    
+    document.getElementById("result").innerHTML =
+    
+    "If you deposit: <mark>" + principal + "</mark><br>\
+    at an interest rate of: <mark>" + rate + "%</mark><br>\
+    You will receive an amount of <mark>" + interest + "</mark><br>\
+    in the year: <mark>" + future_year + "</mark><br>";
+
+    }
+    
+}
+
+function rangeLabel(){
+    document.getElementById("rangeLabelResult").innerHTML = document.getElementById("rate").value + "%";
 }
         
